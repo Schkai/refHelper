cc094b118ff86093e253b1a59352d19867445db9
Konstantin Seitz <konstantinseitz@live.de>
2017-12-01 02:49:34 +0100

    Fixed wrong button action

--- src/pages/pharmacy/pharmacy.ts
+++ src/pages/pharmacy/pharmacy.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { JSONService } from './../../providers/json-service';
 import { LaunchNavigator } from '@ionic-native/launch-navigator';
+import { CallNumber } from '@ionic-native/call-number';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { LaunchNavigator } from '@ionic-native/launch-navigator';
 export class Pharmacy {
 
   public pharmacyData: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private JSONService:JSONService, private launchNavigator: LaunchNavigator) {
+  constructor(public navCtrl: NavController, private callNumber: CallNumber, public navParams: NavParams, private JSONService:JSONService, private launchNavigator: LaunchNavigator) {
   this.getData();
   }
 
@@ -38,6 +39,14 @@ navigate(card){
   let location = card.street + ", " + card.PLZ;
   console.log(location);
   this.launchNavigator.navigate(location);
+}
+
+call(card){
+  let number = card.Telefon;
+  console.log(number);
+  this.callNumber.callNumber(number, true)
+  .then(() => console.log('Launched dialer!'))
+  .catch(() => console.log('Error launching dialer'));
 }
 
 }
